a03a413f10bcb2c13df59acc86a201852aca215b
Luciana Correa <recadoluciana@gmail.com>
2026-06-24 17:46:28 -0300

    trocar os dominios para clubbar

---
+++ js/config.js
@@ -0,0 +1,4 @@
+const API_URL = 'https://api.clubbar.com.br';
+const SITE_URL = 'https://clubbar.com.br';
+const APP_URL = 'https://app.clubbar.com.br';
+const ADMIN_URL = 'https://admin.clubbar.com.br';
