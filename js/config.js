3afe412b6f7b4aeaf2e87126f4b807d3590d6e3e
Luciana Correa <recadoluciana@gmail.com>
2026-07-21 20:51:59 -0300

    config com api em desenvolvimento

--- js/config.js
+++ js/config.js
@@ -1,4 +1,5 @@
-const API_URL = 'https://api.clubbar.com.br';
+const API_URL =
+  'https://apiclubbar-desenvolvimento.up.railway.app';
 const SITE_URL = 'https://clubbar.com.br';
 const APP_URL = 'https://app.clubbar.com.br';
 const ADMIN_URL = 'https://admin.clubbar.com.br';
