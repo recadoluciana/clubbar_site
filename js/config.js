6d18513907ea4ff22c499f49f44b421a99ab70b2
Luciana Correa <recadoluciana@gmail.com>
2026-07-24 11:16:18 -0300

    nova landingpage para o clubbar site 1

--- js/config.js
+++ js/config.js
@@ -1,4 +1,9 @@
 const CLUBBAR_CONFIG = {
   API_BASE_URL: 'https://apiclubbar-desenvolvimento.up.railway.app',
-  ADMIN_URL: 'https://admin.clubbar.com.br/'
+
+  ADMIN_URL: 'https://clubbaradmin-desenvolvimento.up.railway.app',
+
+  CLIENTE_URL: 'https://clubbarcliente-desenvolvimento.up.railway.app',
+
+  AMBIENTE: 'DESENVOLVIMENTO'
 };
