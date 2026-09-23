8ea28b240d182c3184b479f5e0c692bb8157a569
Luciana Correa <recadoluciana@gmail.com>
2026-09-23 14:43:43 -0300

    fix: corrigir endereco do portal partner

--- js/config.js
+++ js/config.js
@@ -16,7 +16,7 @@ const CLUBBAR_CONFIG = CLUBBAR_EM_DESENVOLVIMENTO
   : {
       API_BASE_URL: 'https://apiclubbar-production.up.railway.app',
       ADMIN_URL: 'https://admin.clubbar.com.br',
-      PARTNER_URL: 'https://parceiro.clubbar.com.br',
+      PARTNER_URL: 'https://partner.clubbar.com.br',
       CLIENTE_URL: 'https://app.clubbar.com.br',
       AMBIENTE: 'PRODUCAO',
     };
